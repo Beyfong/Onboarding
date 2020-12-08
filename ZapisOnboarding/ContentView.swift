0fbf5d1c63d747088c24e5d74d55f4d68dd13c05
AMBAL-DAMBAL <ablai2602@gmail.com>
2020-12-09 02:32:56 +0600

    Initial Commit

---
+++ ZapisOnboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZapisOnboarding
+//
+//  Created by ablai erzhanov on 12/9/20.
+//  Copyright © 2020 ablai erzhanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
